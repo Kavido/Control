f07d031df1602ad81574cb9bc829a60f387a98d0
Kala <vdoletskaya@bk.ru>
2022-06-29 15:33:33 +0300

    Задание

--- Control/Program.cs
+++ Control/Program.cs
@@ -1,4 +1,4 @@
-﻿string[] array1 = new string[5] {"467", "12", "help", "Mama", "ola"};
+﻿string[] array1 = new string[7] {"12", "help", "Mama", "ola", "hi", "31.03", "467"};
 string[] array2 = new string[array1.Length];
 void SecondArrayWithIF(string[] array1, string[] array2)
 {
