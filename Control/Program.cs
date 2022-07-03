72ac1a38b0e88bd0589a4e8754dca543c5f570c1
Kala <vdoletskaya@bk.ru>
2022-07-03 10:38:14 +0300

    задание

--- Control/Program.cs
+++ Control/Program.cs
@@ -1,3 +1,14 @@
 ﻿string[] array1 = new string[7] {"12", "help", "Mama", "ola", "hi", "31.03", "467"};
 string[] array2 = new string[array1.Length];
 void SecondArrayWithIF(string[] array1, string[] array2)
+{
+      int count= 0;
+    for (int i = 0; i < array1.Length; i++)
+    {
+    if(array1[i].Length <= 3)
+        {
+        array2[count] = array1[i];
+        count++;
+        }
+    }
+}
